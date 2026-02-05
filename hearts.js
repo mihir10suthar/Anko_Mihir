f800d4b47d8ee1682dba6199d5c645afede454e7
mihir10suthar <mihir10suthar@gmail.com>
2026-02-05 11:19:15 +0530

    Delete hearts.js

--- hearts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("scroll",()=>{
-let h=document.createElement("span");
-h.innerHTML="💗";
-h.style.position="fixed";
-h.style.left=Math.random()*100+"%";
-h.style.top="90%";
-document.body.appendChild(h);
-setTimeout(()=>h.remove(),2000);
-});
